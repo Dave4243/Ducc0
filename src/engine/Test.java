4ea5218aff0f1aed40a81827e9fd0e15f79f5bf0
Dave4243 <davidhuang.oc@gmail.com>
2023-11-24 17:16:38 -0600

    test

--- src/engine/Test.java
+++ src/engine/Test.java
@@ -3,26 +3,70 @@ package engine;
 public class Test {
 	
 	public static void main(String args[]) {
-		MoveGenerator gen = new MoveGenerator();
-		Board b = new Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
-//		System.out.println(new Evaluator().evaluatePosition(b));
+		testPosition("startpos");
+//		Piece p = new Piece(0, 5);
+//		Entry e = new Entry(999999999, new Move(4, 6), 0, 12, 6);
+//		System.out.println("Move size: " + ObjectSizeFetcher.getObjectSize(e.getBestMove()));
+//		System.out.println("Piece size: " + ObjectSizeFetcher.getObjectSize(p));
+//		Board b = new Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+//		b.toString();
+		
 //		Board b = new Board();
+//		MoveGenerator mg = new MoveGenerator();
+//		long start = System.nanoTime();
+//		int totalPositions = moveGenerateTest(6, 0, b, mg);
+//		System.out.println("Total: " + totalPositions);
+//		long end = System.nanoTime();
+//		double totalTime = (end - start)/1000000000.0;
+//		System.out.println("Total Time: " + totalTime);
+//		System.out.println("NPS: " + totalPositions/totalTime);
+		
+//		int move = Move.make(5, 12, Piece.QUEEN);
+//		move = Move.setCap(move, new Piece(1, Piece.PAWN));
+//		System.out.println(move);
+//		System.out.println("From: " + Move.getFrom(move));
+//		System.out.println("To: " + Move.getTo(move));
+//		System.out.println("Promotion: " + Move.getPromotion(move));
+//		System.out.println("Captured: " + Move.getCaptured(move));
+//		System.out.println("En Passant: " + Move.getCastle(move));
+//		System.out.println("Castling flags: " + Move.getCastle(move));
+//		System.out.println(Move.toString(move));
+//		BitBoard.print(move);
+//		System.out.println("===========================");
+//		
+//		TranspositionTable tt = new TranspositionTable();
+//		tt.store(123456789, move, -777, 25, (byte)0);
+//
+//		System.out.println(tt.lookup(123456789).getBestMove());
+//		int ttMove = tt.lookup(123456789).getBestMove();
+//		System.out.println("From: " + Move.getFrom(ttMove));
+//		System.out.println("To: " + Move.getTo(ttMove));
+//		System.out.println("Promotion: " + Move.getPromotion(ttMove));
+//		System.out.println("Captured: " + Move.getCaptured(ttMove));
+//		System.out.println(Move.toString(ttMove));
+//		System.out.println("En Passant: " + Move.getCastle(ttMove));
+//		System.out.println("Castling flags: " + Move.getCastle(ttMove));
+//		BitBoard.print(ttMove);
+//		System.out.println("==================");
+	}
+	
+	private static void testPosition(String fen) {
+		MoveGenerator gen = new MoveGenerator();
+		Board b;
+		if (fen.equals("startpos")) {
+			b = new Board();
+		}
+		else {
+			b = new Board(fen);
+		}
+
 		Search s = new Search(b);
 		long startTime = System.nanoTime();
 		System.out.println(s.getBestMove(b, 600000, 0));
-//		b.makeNullMove();
-//		System.out.println(moveGenerateTest(6, 0, b, gen));
 		long endTime = System.nanoTime();
 		
 		long timeSpent = endTime - startTime;
 		System.out.println(timeSpent/1000000000.0);
-		
-		for (int[] i : Tables.lmrTable) {
-			for (int j : i) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
 	}
 	
 	private static int moveGenerateTest(int depth, int ply, Board b, MoveGenerator gen)
@@ -30,24 +74,23 @@ public class Test {
 		if (depth == 0)
 			return 1;
 
-		
 		MoveList moveList = gen.generateMoves(b, false);
 		
 		int totalPositions= 0;
 		int positionsForMove = 0;
 		
 		for (int i = 0; i < moveList.size(); i++){
-			Move m = moveList.moves[i];
+			int m = moveList.moves[i];
 			if (b.doMove(m)) {
-//				if (b.getZobristKey() != Zobrist.getKey(b)) {
-//					System.out.println("ERRORRR");
-//				}
+				if (b.getZobristKey() != Zobrist.getKey(b)) {
+					System.out.println("ERRORRR");
+				}
 				int numPositions = moveGenerateTest(depth-1, ply +1,  b, gen);
 				totalPositions += numPositions;
 				positionsForMove += numPositions;
 				b.undoMove(m);
 				if (ply == 0) {
-					System.out.println(m + ": " + positionsForMove);
+					System.out.println(Move.toString(m) + ": " + positionsForMove);
 					positionsForMove = 0;
 				}
 			}
